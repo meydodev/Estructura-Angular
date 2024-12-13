97505d7c2afc37755d9023c98a94d02fadb68f3f
meydodev <meydodev@gmail.com>
2024-12-13 23:33:33 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/auth/login/login.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
+
 import { MyComponentComponent } from './components/my-component/my-component.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MySecondComponentComponent } from './components/my-second-component/my-second-component.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    MyComponentComponent,
+    RegisterComponent,
     FooterComponent,
-    MyComponentComponent
+    MySecondComponentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
